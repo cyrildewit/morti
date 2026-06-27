87703ca4a8795ff09874628b5ed38aea61bfe578
Cyril de Wit <cyril@cyrildewit.nl>
2026-06-27 22:35:31 +0200

    feat(apphost): add persistent PostgreSQL container and Morti database

--- src/Morti.AppHost/AppHost.cs
+++ src/Morti.AppHost/AppHost.cs
@@ -1,5 +1,12 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var apiService = builder.AddProject<Projects.Morti_API>("morti-api");
+var postgres = builder.AddPostgres("postgres")
+    .WithLifetime(ContainerLifetime.Persistent);
+
+var mortiDb = postgres.AddDatabase("morti");
+
+var apiService = builder.AddProject<Projects.Morti_API>("morti-api")
+    .WithReference(mortiDb)
+    .WaitFor(mortiDb);
 
 builder.Build().Run();
